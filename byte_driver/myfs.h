73fada63060bb7385771bb812b3abb63671c5512
liuxy <lxy_bonnie@163.com>
2016-08-05 02:00:40 -0700

    v3.0

--- byte_driver/myfs.h
+++
@@ -1,8 +0,0 @@
-    #ifndef MYFS_H  
-    #define MYFS_H  
-    #include "mytypes.h"  
-    #include "myinode.h"  
-    struct file_operations {  
-        int (*open) (struct inode *);  
-    };  
-    #endif 
--- byte_driver/mytypes.h
+++
@@ -1,5 +0,0 @@
-    #ifndef MYTYPES_H  
-    #define MYTYPES_H  
-    typedef unsigned int u32;  
-    typedef u32 dev_t;  
-    #endif  
